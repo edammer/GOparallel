a18caa769a50c35f9413f6cb6243145012450d32
Eric Dammer <edammer@emory.edu>
2022-07-27 07:52:39 -0400

    Update GOparallel-FET.R

--- GOparallel-FET.R
+++ GOparallel-FET.R
@@ -1,4 +1,10 @@
-GOparallel <- function(dummyVar,env=.GlobalEnv) {
+###################################################################################################################################
+## One-Step GSA FET (R piano implementation) WITH USER PARAMETERS
+##  - by Eric Dammer, Divya Nandakumar
+## Nicholas Seyfried Lab Bioinformatics - for the lab - 07/27/2022 version 1.01
+###################################################################################################################################
+
+GOparallel <- function(dummyVar="",env=.GlobalEnv) {
 	## Clean out spaces and escaped backslashes from folder paths (folder names with spaces should not be used on non-windows systems with this script)
 	#filePath=paste0(paste( sapply(do.call(c,strsplit(filePath,"[/\\]")),function(x) { if (grepl(" ",x)) { gsub(x,paste0(substr(gsub(" ","",x),1,6),"~1"),x) } else { x } } ),collapse="/"),"/")
 	filePath=paste0(paste( sapply(do.call(c,strsplit(filePath,"[/\\]")),function(x) { if (grepl(" ",x)) { gsub(" ","\ ",x) } else { x } } ),collapse="/"),"/")
