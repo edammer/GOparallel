dbac4289a3808a5c3e6c6162092b05470fa63071
Eric Dammer <edammer@emory.edu>
2023-01-28 13:51:59 -0500

    Update GOparallel-FET.R

--- GOparallel-FET.R
+++ GOparallel-FET.R
@@ -186,7 +186,7 @@ GOparallel <- function(dummyVar="",env=.GlobalEnv) {
 	if(!exists("corVolc")) corVolc=FALSE
 	if (ANOVAgroups) {
 
-	  if(corVolc & exists("CORout")) cat("- corVolc=TRUE so getting gene lists from significant correlations in statistics table stored in variable CORout.\n"); ANOVAout<-CORout;
+	  if(corVolc & exists("CORout")) { cat("- corVolc=TRUE so getting gene lists from significant correlations in statistics table stored in variable CORout.\n"); ANOVAout<-CORout; }
 	  if(corVolc & !exists("CORout")) if (!length(dummyVar)==1) { cat("- corVolc=TRUE, but CORout not in memory, using input provided to this function.\n"); ANOVAout=as.data.frame(dummyVar); } else { stop("Variable CORout not found or no input was provided.\nPlease run trait.corStat() function first, and save output to CORout variable or pass its output to this function.\n\n") }
 
 	  if (!exists("ANOVAout")) if (!length(dummyVar)==1) { cat("- ANOVAout not in memory, trying to use input provided to this function.\n"); ANOVAout=as.data.frame(dummyVar); } else { stop("Variable ANOVAout not found or no input was provided.\nPlease run parANOVA.dex() or trait.corStat() function first, and save output to ANOVAout variable or pass its output to this function.\n\n") }
